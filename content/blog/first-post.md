a73094d3540eea4f4ac40a0386100103284eb878
Mauritz Vervaart <mjver@mweb.co.za>
2022-03-23 21:16:31 +0200

    first commit

---
+++ content/blog/first-post.md
@@ -0,0 +1,12 @@
+---
+title: My First Blog Post
+description: Welcome to my first blog post using the content module.
+slug: first-post
+img: blogimg1.jpg
+---
+
+# My first blog post
+
+My first blog post displayed here
+
+The lorem ipsum is a placeholder text used in publishing and graphic design. This filler text is a short paragraph that contains all the letters of the alphabet. The characters are spread out evenly so that the reader's attention is focused on the layout of the text instead of its content
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,31 @@
+<template>
+  <article>
+      <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+export default {
+    async asyncData ({ $content, params }) {
+        const article = await $content('blog', params.slug).fetch()
+
+        return { article }
+    }
+}
+</script>
+
+<style>
+article {
+    display: block;
+    margin: 0 auto;
+    padding: 50px 30px;
+    max-width: 800px;
+}
+
+h1 {
+    font-weight: 900;
+    font-size: 28px;
+    margin-bottom: 30px;
+}
+
+</style>
